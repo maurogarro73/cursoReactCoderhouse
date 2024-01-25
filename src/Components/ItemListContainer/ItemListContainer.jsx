b0d3f93510c19536fe19c71bc92feab1ae0b92eb
Mauro Garro <maurogarro73@gmail.com>
2024-01-25 19:47:53 -0300

    itemCount fin

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
 
 function ItemListContainer({ greeting }) {
-  return <div className="mt-10 text-center m-auto font-bold text-3xl text-sky-700">{greeting}</div>;
+  return (
+    <div>
+      <p className="mt-10 text-center m-auto font-bold text-3xl text-sky-700">{greeting}</p>
+      <ItemCount stock={5} initial={1} />
+    </div>
+  );
 }
 
 export default ItemListContainer;
