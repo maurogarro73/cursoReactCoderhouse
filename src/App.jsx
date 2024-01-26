df3b8e6312e1e9f981a9d66a788a00d31f10ec73
Mauro Garro <maurogarro73@gmail.com>
2024-01-26 20:37:07 -0300

    daisyUi integrado

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import './App.css';
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 import NavBar from './Components/NavBar/NavBar';
-import { Count } from './Count';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Count />
       <ItemListContainer greeting="Bienvenidos al futuro...!" />
     </div>
   );
--- src/Components/ItemCount/ItemCount.jsx
+++ src/Components/ItemCount/ItemCount.jsx
@@ -18,24 +18,16 @@ const ItemCount = ({ stock, initial }) => {
   return (
     <div className="flex justify-center ">
       <div className="m-2">
-        <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          onClick={handlerResta}
-        >
+        <button className="btn btn-accent" onClick={handlerResta}>
           -
         </button>
         <span className="text-center px-5">{count}</span>
-        <button
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          onClick={handlerSuma}
-        >
+        <button className="btn btn-accent" onClick={handlerSuma}>
           +
         </button>
       </div>
       <div>
-        <button className="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-          Agregar al carrito
-        </button>
+        <button className="btn btn-accent my-2">Agregar al carrito</button>
       </div>
     </div>
   );
